66d074b9e86f4de6317629a8511cba131f088225
yckao <yckaokarl@gmail.com>
2017-07-06 18:23:54 +0800

    Complete Announcement Model

--- CCIP-iOS-Swift/CCIP-iOS-Swift/Models/Announcement.swift
+++ CCIP-iOS-Swift/CCIP-iOS-Swift/Models/Announcement.swift
@@ -7,3 +7,33 @@
 //
 
 import Foundation
+import ObjectMapper
+
+class Announcement: Mappable {
+    
+    var datetime: Date!
+    var msgZh: String?
+    var msgEn: String?
+    var uri: String?
+    
+    var msg: String {
+        get {
+            if (NSLocale.current.identifier == "zh") {
+                return msgZh!
+            }
+            return msgEn!
+        }
+    }
+    
+    // MARK: ObjectMapper
+    required init?(map: Map) {
+        
+    }
+    
+    func mapping(map: Map) {
+        datetime <- (map["datetime"], DateTransform())
+        msgZh <- map["msg_zh"]
+        msgEn <- map["msg_en"]
+        uri <- map["uri"]
+    }
+}
